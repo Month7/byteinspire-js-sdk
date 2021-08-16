3131313e41736d76bd00bbe386047e19ec9ee52f
Margaux7 <Margaux7222@gmail.com>
2021-08-16 17:17:19 +0800

    chore: action update readme (#18)

--- scripts/updateEntryFile.js
+++ scripts/updateEntryFile.js
@@ -2,22 +2,25 @@ const fs = require('fs');
 const version = require('../package.json').version;
 
 function updateEntryFile() {
-  let body = fs.readFileSync(`${process.cwd()}/package.json`, 'utf-8');
+  const path = `${process.cwd()}/package.json`;
+  let body = fs.readFileSync(path, 'utf-8');
   body = body.replace(
     /\.\/dist\/inspirecloud.*\.js/g,
     `./dist/inspirecloud.min-${version}.js`
   );
-  fs.writeFileSync(`${process.cwd()}/package.json`, body);
+  fs.writeFileSync(path, body);
 }
+
 function updateReadme() {
-  let body = fs.readFileSync(`${process.cwd()}/README.MD`, 'utf-8');
+  const path = `${process.cwd()}/README.md`;
+  let body = fs.readFileSync(path, 'utf-8');
   body = body
     .replace(
       /https:\/\/unpkg\.com.*?\.js/g,
       `https://unpkg.com/@byteinspire/js-sdk@${version}/dist/inspirecloud.min-${version}.js`
     )
     .replace(/-\d\.\d\.\d.*?\.js/g, `-${version}.js`);
-  fs.writeFileSync(`${process.cwd()}/README.MD`, body);
+  fs.writeFileSync(path, body);
 }
 
 updateReadme();
